46aebcad70b6e2db26393671f03b87592638c4f0
Leon <epicgenii18@gmail.com>
2022-05-02 19:46:02 +0100

    added pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      script: './index.js',
+      instances: '1',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
